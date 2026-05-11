1d14bd4c86ebbf353f3af985f0eeed0f8312f3ff
WillemCartwright <willem1cartwright@gmail.com>
2026-05-11 17:23:36 +0200

    clipboard bezig

---
+++ Assets/EHBO-VR/Scripts/clipboard/ClipboardVergroten.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ClipboardScaleController : MonoBehaviour
+{
+    [Header("Anchors")]
+    public Transform normalAnchor;
+    public Transform focusAnchor;
+
+    [Header("Instellingen")]
+    public float transitionSpeed = 15f; // Iets sneller voor minder 'lag'
+    public InputActionProperty toggleAction;
+
+    private bool isFocused = false;
+
+    void OnEnable() => toggleAction.action.Enable();
+
+    void LateUpdate() // LateUpdate werkt beter voor objecten die de camera volgen
+    {
+        if (toggleAction.action.WasPressedThisFrame())
+        {
+            isFocused = !isFocused;
+            Debug.Log("Switching Clipboard. Focused: " + isFocused);
+        }
+
+        Transform target = isFocused ? focusAnchor : normalAnchor;
+
+        if (target != null)
+        {
+            // Gebruik MoveTowards of een snelle Lerp voor stabiliteit
+            transform.position = Vector3.Lerp(transform.position, target.position, Time.deltaTime * transitionSpeed);
+            transform.rotation = Quaternion.Slerp(transform.rotation, target.rotation, Time.deltaTime * transitionSpeed);
+            transform.localScale = Vector3.Lerp(transform.localScale, target.localScale, Time.deltaTime * transitionSpeed);
+        }
+    }
+}
