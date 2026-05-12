0fe7859e0dedca3c7023cc7f90b0286c4acc28f7
WillemCartwright <willem1cartwright@gmail.com>
2026-05-12 16:59:06 +0200

    Clipboard werkt, bezig met achievement integratie

--- Assets/EHBO-VR/Scripts/Achievement/Achievement.cs
+++ Assets/EHBO-VR/Scripts/Achievement/Achievement.cs
@@ -38,13 +38,13 @@ public class Achievement : MonoBehaviour
                 switch (completedStep)
                 {
                     case 1:
-                        achievementText.text = "<size=130%><b>De tijd tikt</b></size>\n<size=90%>Prestatie behaald</size>\nTIjdsbalk onder controle";
+                        //achievementText.text = "<size=130%><b>De tijd tikt</b></size>\n<size=90%>Prestatie behaald</size>\nTIjdsbalk onder controle";
                         break;
                     case 2:
-                        achievementText.text = "<size=130%><b>Aan de slag</b></size>\n<size=90%>Prestatie behaald</size>\nVR-handen onder controle";
+                        //achievementText.text = "<size=130%><b>Aan de slag</b></size>\n<size=90%>Prestatie behaald</size>\nVR-handen onder controle";
                         break;
                     case 3:
-                        achievementText.text = "<size=130%><b>EHBO in de praktijk</b></size>\n<size=90%>Prestatie behaald</size>\nHandgebaren onder controle";
+                        //achievementText.text = "<size=130%><b>EHBO in de praktijk</b></size>\n<size=90%>Prestatie behaald</size>\nHandgebaren onder controle";
                         break;
                 }
                 break;
@@ -53,13 +53,13 @@ public class Achievement : MonoBehaviour
                 switch (completedStep)
                 {
                     case 1:
-                        achievementText.text = "<size=130%><b>VR Task 1</b></size>\n<size=90%>Prestatie behaald</size>\nKubus is opgepakt";
+                        //achievementText.text = "<size=130%><b>VR Task 1</b></size>\n<size=90%>Prestatie behaald</size>\nKubus is opgepakt";
                         break;
                     case 2:
-                        achievementText.text = "<size=130%><b>VR Task 2</b></size>\n<size=90%>Prestatie behaald</size>\nKnop ingedrukt";
+                        //achievementText.text = "<size=130%><b>VR Task 2</b></size>\n<size=90%>Prestatie behaald</size>\nKnop ingedrukt";
                         break;
                     case 3:
-                        achievementText.text = "<size=130%><b>VR Task 3</b></size>\n<size=90%>Prestatie behaald</size>\nStop teken gegeven";
+                        //achievementText.text = "<size=130%><b>VR Task 3</b></size>\n<size=90%>Prestatie behaald</size>\nStop teken gegeven";
                         break;
                 }
                 break;
